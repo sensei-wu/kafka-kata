4aac67fec2592f192cdbe5fa6ba216d424332df1
Zenifer Cheruveettil <Zenifer.Cheruveettil@booxware.de>
2018-06-17 08:24:09 +0200

    added comment

--- src/main/java/com/zen/lab/dojo/kafka/KafkaConfiguration.java
+++ src/main/java/com/zen/lab/dojo/kafka/KafkaConfiguration.java
@@ -21,8 +21,12 @@ public class KafkaConfiguration {
     private Properties createProperties() {
         Properties properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        /* setting the below make sure that only once message is written to all in sync replicas, it is considered committed
+          default value of "acks" is 1, which means a message is considered committed once it is successfully written to leader
+          setting this value to 0 might result in message losses as in that case, messages are considered committed before any of the
+          server acknowledges the commit
+        */
         properties.put(ProducerConfig.ACKS_CONFIG, "all");
-     //   properties.put(ProducerConfig.)
         return properties;
     }
 }
